498940e50417a8afb8f6cc74a6057bac749ef71a
Gabriel Rascovit <rascovit@gmail.com>
2018-06-24 16:39:47 -0300

    add username to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable,
     :rememberable, :trackable, :validatable, :confirmable
+
+  validates :username, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,8 @@ puts 'Creating users...'
 
 5.times do |i|
   User.create!(
-    email: "user_#{i}@tester.com",
+    username: "user#{i}",
+    email: "user#{i}@tester.com",
     password: 'thecops'
   )
 end
