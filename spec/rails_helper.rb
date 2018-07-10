8fc897c3e2d8f73b600cb002e8426c4a91c50768
Thiago Durante Pires <thdurante@gmail.com>
2018-07-09 22:07:10 -0300

    Ignore application_controller in coverage report (temporarily).

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,7 @@ if ENV['coverage'] == 'on'
     add_filter 'app/views'
     add_filter 'app/jobs/application_job.rb'
     add_filter 'app/mailers/application_mailer.rb'
+    add_filter 'app/controllers/application_controller.rb' # TODO: remove after application layout is used.
     add_filter 'app/models/application_record.rb'
     minimum_coverage 100
   end
