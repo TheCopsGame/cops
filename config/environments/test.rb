71340e329f0bcfd0f5ea1cd466c3cd7a57b79fbc
Gabriel Rascovit <rascovit@gmail.com>
2018-07-09 21:14:08 -0300

    create specs for user login and sign up

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,11 +31,9 @@ Rails.application.configure do
   # Store uploaded files on the local file system in a temporary directory
   config.active_storage.service = :test
 
+  # ActionMailer
   config.action_mailer.perform_caching = false
-
-  # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :test
 
   # Print deprecation notices to the stderr.
