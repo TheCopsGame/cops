25db2f1f3140e039ca3ce9642e562364ea317555
Gabriel Rascovit <rascovit@gmail.com>
2018-09-20 23:32:43 -0300

    add character relationship to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable,
     :rememberable, :trackable, :validatable, :confirmable
 
+  has_one :character, dependent: :destroy
+
   validates :username, presence: true
 end
