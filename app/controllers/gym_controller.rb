4157281d668c4fa003485fa21eb515227a9871f3
Gabriel Rascovit <rascovit@gmail.com>
2018-10-15 23:31:38 -0300

    create service for gym

--- app/controllers/gym_controller.rb
+++ app/controllers/gym_controller.rb
@@ -1,5 +1,20 @@
 class GymController < ApplicationController
   def index; end
 
-  def train; end
+  def train
+    Cops::TrainingManager.call(
+      character: current_character,
+      item: TrainingKind.coerce(params[:training_kind]),
+      cost_attrs: { cost: :energy },
+      gain_attrs: { strength: :strength, agility: :agility }
+    )
+
+    redirect_with_message(path: gym_path, kind: :notice, message: t('.success'))
+  rescue Cops::TrainingManager::InvalidParamsError
+    redirect_with_message(path: gym_path, kind: :alert, message: t('.invalid_params'))
+  rescue Cops::TrainingManager::InsufficientEnergyError
+    redirect_with_message(path: gym_path, kind: :alert, message: t('.insufficient_energy'))
+  rescue Cops::TrainingManager::CharacterValidationError
+    redirect_with_message(path: gym_path, kind: :alert, message: t('.invalid_character'))
+  end
 end
