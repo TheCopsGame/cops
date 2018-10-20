d580dc28c852e9fe9e5bce846cfc4324609e002d
Thiago Durante Pires <thdurante@gmail.com>
2018-10-20 15:41:43 -0300

    Add images to SnackKind enumerator.

--- app/enumerators/snack_kind.rb
+++ app/enumerators/snack_kind.rb
@@ -1,14 +1,14 @@
 class SnackKind
   include EnumeratedTypeWithBenefits
 
-  declare :sandwich,  cost: 5,  energy: 10
-  declare :soda,      cost: 2,  energy: 5
-  declare :pizza,     cost: 12, energy: 22
-  declare :candy_bar, cost: 3,  energy: 5
-  declare :pancake,   cost: 7,  energy: 16
-  declare :cookie,    cost: 3,  energy: 5
-  declare :coffee,    cost: 2,  energy: 10
-  declare :steak,     cost: 15, energy: 25
-  declare :juice,     cost: 8,  energy: 13
-  declare :milkshake, cost: 9,  energy: 19
+  declare :sandwich,  cost: 5,  energy: 10, image: 'snack/burger.png'
+  declare :soda,      cost: 2,  energy: 5,  image: 'snack/soda.png'
+  declare :pizza,     cost: 12, energy: 22, image: 'snack/pizza.png'
+  declare :candy_bar, cost: 3,  energy: 5,  image: 'snack/candy.png'
+  declare :pancake,   cost: 7,  energy: 16, image: 'snack/pancake.png'
+  declare :cookie,    cost: 3,  energy: 5,  image: 'snack/cookies.png'
+  declare :coffee,    cost: 2,  energy: 10, image: 'snack/hot-drink.png'
+  declare :steak,     cost: 15, energy: 25, image: 'snack/steak.png'
+  declare :juice,     cost: 8,  energy: 13, image: 'snack/juice.png'
+  declare :milkshake, cost: 9,  energy: 19, image: 'snack/milkshake.png'
 end
