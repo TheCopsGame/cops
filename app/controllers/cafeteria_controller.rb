32190380e40c32e6afafe23ec9a5602d50e45063
Thiago Durante Pires <thdurante@gmail.com>
2018-10-20 18:31:53 -0300

    Fix controller and services.

--- app/controllers/cafeteria_controller.rb
+++ app/controllers/cafeteria_controller.rb
@@ -6,17 +6,15 @@ class CafeteriaController < ApplicationController
   def purchase_snack
     Cops::PurchaseManager.call(
       character: current_character,
-      item: SnackKind.coerce(params[:snack_kind]),
-      cost_attrs: { cost: :money },
-      gain_attrs: { energy: :energy }
+      item: SnackKind.coerce(params[:snack_kind])
     )
 
-    redirect_with_message(path: cafeteria_path, kind: :notice, message: :success)
+    redirect_with_message(path: cafeteria_path, kind: :notice, message: t('.success'))
   rescue Cops::PurchaseManager::InvalidParamsError
-    redirect_with_message(path: cafeteria_path, kind: :alert, message: :insufficient_params)
+    redirect_with_message(path: cafeteria_path, kind: :alert, message: t('.invalid_params'))
   rescue Cops::PurchaseManager::InsufficientFundsError
-    redirect_with_message(path: cafeteria_path, kind: :alert, message: :insufficient_funds)
+    redirect_with_message(path: cafeteria_path, kind: :alert, message: t('.insufficient_funds'))
   rescue Cops::PurchaseManager::CharacterValidationError
-    redirect_with_message(path: cafeteria_path, kind: :alert, message: :invalid_character)
+    redirect_with_message(path: cafeteria_path, kind: :alert, message: t('.invalid_character'))
   end
 end
