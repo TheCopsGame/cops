88945be1976899ff5d6f2b96515275324d6e5152
Gabriel Rascovit <rascovit@gmail.com>
2018-10-22 22:07:57 -0200

    refactor to match rubocop rule

--- app/jobs/energy_recovery_job.rb
+++ app/jobs/energy_recovery_job.rb
@@ -3,7 +3,10 @@ class EnergyRecoveryJob < ApplicationJob
 
   def perform
     Character.find_each do |character|
-      character.increment!(:energy, 1) if character.energy < 100
+      next unless character.energy < 100
+
+      character.increment(:energy)
+      character.save
     end
 
     EnergyRecoveryJob.set(wait: 15.seconds).perform_later
