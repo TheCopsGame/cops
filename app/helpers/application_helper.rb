3e9b6422202d0ead74edd4e6aea299f2bd41cdb1
Gabriel Rascovit <rascovit@gmail.com>
2018-10-23 22:02:54 -0200

    different colors for each flash message type

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_message_class(type)
+    case type
+    when 'notice'
+      'positive'
+    when 'alert'
+      'negative'
+    else
+      ''
+    end
+  end
 end
