5b38ecb2fe87845f788382b2521cd8de89a8987d
Gabriel Rascovit <rascovit@gmail.com>
2018-10-28 17:39:05 -0200

    add two more kinds of job

--- app/enumerators/job_kind.rb
+++ app/enumerators/job_kind.rb
@@ -9,4 +9,22 @@ class JobKind
     agility: nil,
     intelligence: nil,
     image: 'job/warehouse.png'
+
+  declare :paper_work,
+    experience: 10,
+    energy: -10,
+    money: 20,
+    strength: nil,
+    agility: nil,
+    intelligence: nil,
+    image: 'job/paper_work.png'
+
+  declare :patrol,
+    experience: 15,
+    energy: -15,
+    money: 30,
+    strength: nil,
+    agility: nil,
+    intelligence: nil,
+    image: 'job/patrol.png'
 end
