f7ed8cb7dd552a7e56d6bc3dcfbe4126cea081d1
Gabriel Rascovit <rascovit@gmail.com>
2018-11-26 10:00:56 -0200

    fix background height using css instead of js

--- app/assets/javascripts/auth/auth.js
+++
@@ -1,7 +0,0 @@
-function setHeaderHeight() {
-  var windowHeight = $(window).height()
-  $('#header').css('height', windowHeight + 'px')
-}
-
-$(window).resize(function() { setHeaderHeight() })
-$(document).ready(function() { setHeaderHeight() })
